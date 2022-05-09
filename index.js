00c7be1f379fb51610798fec1dd072a1852d16a6
Samuel Jara <spjara.aravena@gmail.com>
2022-05-09 18:48:33 -0400

    Partials, helpers e interacciones con carrito

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const expresss = require('express');
+const express = require('express');
 const app = express();
 const hbs = require('express-handlebars');
 
-app.set("view-engine", "handlebars");
+app.set("view engine", "handlebars");
 
-app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist/`))
+app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist/`));
+app.use('/jquery', express.static(`${__dirname}/node_modules/jquery/dist/`));
+app.use(express.static(`${__dirname}/public`));
 
 app.engine(
     "handlebars",
@@ -17,5 +19,14 @@ app.engine(
 app.listen(3000, () => console.log("Servidor activo http://localhost:3000"));
 
 app.get("/", (req, res) => {
-    res.render("main");
+    res.render("main", {
+        productos: [
+            "banana",
+            "cebollas",
+            "pimenton",
+            "papas",
+            "lechuga",
+            "tomate"
+        ]
+    });
 });
